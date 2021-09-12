7fbf682bb46cc80eef60482e15f0ea769af1d187
SoaresAlexandre <alexandre1328.soares@gmail.com>
2021-09-12 12:00:56 -0300

    Basic Static Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
-function App(){
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import DataTable from "components/DataTable";
+
+
+function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá Mundo!, Alexandre</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+      <DataTable />
+      </div>
+      <Footer />
+    </>
+
   );
 }
 
